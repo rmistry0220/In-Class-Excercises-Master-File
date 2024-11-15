0fabc46fc6ad150f4763234040743097e540a0c6
rmistry0220 <rrm80@georgetown.edu>
2024-11-15 00:40:16 -0500

    Revert "Stocks.qc (#2)" (#3)
    This reverts commit aebb9cb1a6b3e8b1f5a5a2547b4746ce773d7b2a.

--- app/unemployment.py
+++ app/unemployment.py
@@ -3,14 +3,18 @@
 
 # LOCAL DEV (ENV VARS)
 
-
+import os
 import json
 from pprint import pprint
 from statistics import mean
+from dotenv import load_dotenv
 import requests
 from plotly.express import line
 
-from app.alpha_service import API_KEY
+load_dotenv() # looks in the ".env" file for env vars
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+
 
 def fetch_unemployment_json():
 
